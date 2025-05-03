b6f8fa2b2d4e2d4d68f0bdd02f1b945b270fb82f
karthikpohane <kartikpohane0612@gmail.com>
2025-05-03 23:07:00 +0530

    BE link added

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -6,5 +6,5 @@ import axios from 'axios';
 // Creating and exporting an axios instance with predefined configurations
 export default axios.create({
   // Base URL for API requests. You can update this if the app is deployed to a different domain
-  baseURL: 'https://time-pass-games-log-book-api.vercel.app/',
+  baseURL: 'https://time-pass-games-log-book.vercel.app/',
 });
